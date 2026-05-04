5c74132fa6d10e2376111c30bae4b61f6a5c64fa
junaid Ntd <junaidntd@gmail.com>
2026-05-04 11:03:04 +0530

    update

--- counter.js
+++ counter.js
@@ -1,29 +1,58 @@
-const { createStore } = require("redux");
+import { createStore, combineReducers } from "redux";
 
-const initialState = { count: 0 };
 
-function counterReducer(state = initialState, action) {
+const counterInitialState = { count: 0 };
+
+function counterReducer(state = counterInitialState, action) {
   switch (action.type) {
     case "INCREMENT":
-      return { ...state, count: state.count + 1 };
+      return { count: state.count + 1 };
 
     case "DECREMENT":
-      return { ...state, count: state.count - 1 };
-
-    case "INCREMENT_BY":
-      return { ...state, count: state.count + action.payload };
+      return { count: state.count - 1 };
 
     default:
       return state;
   }
 }
 
-const store = createStore(counterReducer);
 
-store.subscribe(() => {
-  console.log("State:", store.getState());
+const authInitialState = { user: null };
+
+function authReducer(state = authInitialState, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return { user: action.payload };
+
+    case "LOGOUT":
+      return { user: null };
+
+    default:
+      return state;
+  }
+}
+
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  auth: authReducer,
 });
 
+
+const store = createStore(rootReducer);
+
+
+store.subscribe(() => {
+  console.log("Updated State:", store.getState());
+});
+
+
 store.dispatch({ type: "INCREMENT" });
-store.dispatch({ type: "INCREMENT_BY", payload: 10 });
+store.dispatch({ type: "INCREMENT" });
 store.dispatch({ type: "DECREMENT" });
+
+store.dispatch({ type: "LOGIN", payload: "Junaid" });
+store.dispatch({ type: "LOGOUT" });
+
+
+console.log("Final State:", store.getState());
